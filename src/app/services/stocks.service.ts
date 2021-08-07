64e2331a1cd26d88b0bbb1f8db60486ff7bc5258
Benoît Hubert <benoithubert@gmail.com>
2021-08-07 08:08:51 +0200

    Persist symbols list to localStorage

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -2,7 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+const LS_SYMBOLS_KEY: string = 'ng-stocks:symbols';
+
+function getStocksFromStorage(): Array<string> {
+  const defaultStocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+  const storedStocksJSON: string | null = localStorage.getItem(LS_SYMBOLS_KEY);
+  let storedStocks: Array<string>;
+  if (storedStocksJSON === null) {
+    return defaultStocks;
+  }
+  try {
+    storedStocks = JSON.parse(storedStocksJSON);
+    return storedStocks;
+  } catch (err) {
+    return defaultStocks;
+  }
+}
+
+const stocks: Array<string> = getStocksFromStorage();
 const service: string = 'https://angular2-in-action-api.herokuapp.com';
 
 export interface StockInterface {
@@ -25,13 +42,19 @@ export class StocksService {
     return stocks.slice();
   }
 
+  store() {
+    localStorage.setItem(LS_SYMBOLS_KEY, JSON.stringify(this.get()));
+  }
+
   add(stock: string) {
     stocks.push(stock);
+    this.store();
     return this.get();
   }
 
   remove(stock: string) {
     stocks.splice(stocks.indexOf(stock), 1);
+    this.store();
     return this.get();
   }
 
